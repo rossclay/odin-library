9ce5d3cc6512f50b2cb403e5811c9a86c0d53a90
Ross Clay <clay.rossclay@gmail.com>
2024-05-08 12:46:44 -0400

    adds basic functionality to book constructor

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const myLibrary = [];
 
-function Book() {
-    // the constructor...
+
+// the constructor...
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function () {
+        let infoMessage = `${this.title} by ${this.author},  ${this.pages} pages, ${this.read}.`
+        return infoMessage
+    }
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(bookName) {
+    let bookCount = myLibrary.length
+    myLibrary[bookCount] = bookName
 }
+// default value for an example
+const favoriteBook = new Book('No Country For Old Men', 'Cormac McCarthy', 309, 'read')
+addBookToLibrary(favoriteBook)
+
+let cardContainer = document.querySelector('.card-container')
+let cards = document.querySelectorAll('.card')
+
+// function displayBooks() {
+//     for (books in myLibrary[books]) {
+
+//     }
+// }
